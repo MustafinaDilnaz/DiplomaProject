c691f1fa568dbb1168d4aa3ff50f70e836fece79
must_dilnaz <must.dilnaz@gmail.com>
2024-04-12 09:49:09 +0500

    Add my diploma project

---
+++ MyDiplomaProject/MyDiplomaProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MyDiplomaProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
